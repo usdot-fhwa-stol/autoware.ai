9b396febd86743a10ea882e559e82f431f0f8e43
Kyle Rush <kyle.rush@leidos.com>
2022-04-11 08:26:17 -0400

    Updating version numbers for CARMA release

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -35,13 +35,13 @@ done
 cd ${dir}/autoware.ai
 
 if [[ "$BRANCH" = "develop" ]]; then
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/jetta-gillet
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch release/jetta-gillet
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/autoware.auto.git --branch release/jetta-gillet
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch carma-system-4.0.0
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch carma-system-4.0.0
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/autoware.auto.git --branch carma-system-4.0.0
 else
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/jetta-gillet
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch release/jetta-gillet
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/autoware.auto.git --branch release/jetta-gillet
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch carma-system-4.0.0
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch carma-system-4.0.0
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/autoware.auto.git --branch carma-system-4.0.0
 fi
 
 # Required to build pacmod_msgs
