a6835f6ed2815c89ffcd46eb5cc504d1aaa19d29
Misheel Bayartsengel <misheel.bayart@gmail.com>
2023-06-13 20:57:09 +0000

    integration test left

--- core_perception/points_downsampler/nodes/random_filter/random_filter.cpp
+++ core_perception/points_downsampler/nodes/random_filter/random_filter.cpp
@@ -1,63 +1,77 @@
 /*
- * Copyright 2015-2019 Autoware Foundation. All rights reserved.
+ * Copyright (C) 2023 LEIDOS.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
+#include "random_filter.hpp"
 
-#include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
+namespace random_filter
+{
 
-#include "autoware_config_msgs/ConfigRandomFilter.h"
+namespace std_ph = std::placeholders;
 
-#include <points_downsampler/PointsDownsamplerInfo.h>
+RandomFilter::RandomFilter(const rclcpp::NodeOptions& options) : carma_ros2_utils::CarmaLifecycleNode(options)
+{
+  declare_parameter<std::string>("points_topic", POINTS_TOPIC);
+  declare_parameter<bool>("output_log", _output_log);
+  declare_parameter<double>("measurement_range", measurement_range);
+}
 
-#include <chrono>
+carma_ros2_utils::CallbackReturn RandomFilter::handle_on_configure(const rclcpp_lifecycle::State &)
+{
+  get_parameter<std::string>("points_topic", POINTS_TOPIC);
+  get_parameter<bool>("output_log", _output_log);
 
-#include "points_downsampler.h"
+  if(_output_log == true){
+	  char buffer[80];
+	  std::time_t now = std::time(NULL);
+	  std::tm *pnow = std::localtime(&now);
+	  std::strftime(buffer,80,"%Y%m%d_%H%M%S",pnow);
+	  filename = "random_filter_" + std::string(buffer) + ".csv";
+	  ofs.open(filename.c_str(), std::ios::app);
+  }
+  measurement_range = MAX_MEASUREMENT_RANGE;
+  get_parameter<double>("measurement_range", measurement_range);
 
-#define MAX_MEASUREMENT_RANGE 200.0
+  // Publishers
+  filtered_points_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("/filtered_points", 10);
+  points_downsampler_info_pub_ = create_publisher<points_downsampler::msg::PointsDownsamplerInfo>("/points_downsampler_info", 1000);
 
-ros::Publisher filtered_points_pub;
+  // Subscribers
+  config_sub_ = create_subscription<autoware_config_msgs::msg::ConfigRandomFilter>("config/random_filter", 10, std::bind(&RandomFilter::config_callback, this, std_ph::_1));
+  scan_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(POINTS_TOPIC, 10, std::bind(&RandomFilter::scan_callback, this, std_ph::_1));
 
-static int sample_num = 1000;
+  return CallbackReturn::SUCCESS;
+}
 
-static ros::Publisher points_downsampler_info_pub;
-static points_downsampler::PointsDownsamplerInfo points_downsampler_info_msg;
+carma_ros2_utils::CallbackReturn RandomFilter::handle_on_activate(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
 
-static std::chrono::time_point<std::chrono::system_clock> filter_start, filter_end;
-
-static bool _output_log = false;
-static std::ofstream ofs;
-static std::string filename;
-
-static std::string POINTS_TOPIC;
-static double measurement_range = MAX_MEASUREMENT_RANGE;
-
-static void config_callback(const autoware_config_msgs::ConfigRandomFilter::ConstPtr& input)
+void RandomFilter::config_callback(autoware_config_msgs::msg::ConfigRandomFilter::UniquePtr input)
 {
   sample_num = input->sample_num;
   measurement_range = input->measurement_range;
 }
 
-static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
+void RandomFilter::scan_callback(sensor_msgs::msg::PointCloud2::UniquePtr input)
 {
   pcl::PointXYZI sampled_p;
   pcl::PointCloud<pcl::PointXYZI> scan;
 
-  pcl::fromROSMsg(*input, scan);
+  //pcl::fromROSMsg(*input, scan);
 
   if(measurement_range != MAX_MEASUREMENT_RANGE){
     scan = removePointsByRange(scan, 0, measurement_range);
@@ -84,13 +98,13 @@ static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
     filtered_scan_ptr = scan.makeShared();
   }
 
-  sensor_msgs::PointCloud2 filtered_msg;
-  pcl::toROSMsg(*filtered_scan_ptr, filtered_msg);
+  sensor_msgs::msg::PointCloud2 filtered_msg;
+  //pcl::toROSMsg(*filtered_scan_ptr, filtered_msg);
 
   filter_end = std::chrono::system_clock::now();
 
   filtered_msg.header = input->header;
-  filtered_points_pub.publish(filtered_msg);
+  filtered_points_pub_->publish(filtered_msg);
 
   points_downsampler_info_msg.header = input->header;
   points_downsampler_info_msg.filter_name = "random_filter";
@@ -100,15 +114,14 @@ static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
   points_downsampler_info_msg.original_ring_size = 0;
   points_downsampler_info_msg.filtered_ring_size = 0;
   points_downsampler_info_msg.exe_time = std::chrono::duration_cast<std::chrono::microseconds>(filter_end - filter_start).count() / 1000.0;
-  points_downsampler_info_pub.publish(points_downsampler_info_msg);
+  points_downsampler_info_pub_->publish(points_downsampler_info_msg);
 
   if(_output_log == true){
 	  if(!ofs){
 		  std::cerr << "Could not open " << filename << "." << std::endl;
 		  exit(1);
 	  }
-	  ofs << points_downsampler_info_msg.header.seq << ","
-		  << points_downsampler_info_msg.header.stamp << ","
+	  ofs << std::to_string(rclcpp::Time(points_downsampler_info_msg.header.stamp).seconds()) << ","
 		  << points_downsampler_info_msg.header.frame_id << ","
 		  << points_downsampler_info_msg.filter_name << ","
 		  << points_downsampler_info_msg.original_points_size << ","
@@ -121,34 +134,4 @@ static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
 
 }
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "random_filter");
-
-  ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
-
-  private_nh.getParam("points_topic", POINTS_TOPIC);
-  private_nh.getParam("output_log", _output_log);
-  if(_output_log == true){
-	  char buffer[80];
-	  std::time_t now = std::time(NULL);
-	  std::tm *pnow = std::localtime(&now);
-	  std::strftime(buffer,80,"%Y%m%d_%H%M%S",pnow);
-	  filename = "random_filter_" + std::string(buffer) + ".csv";
-	  ofs.open(filename.c_str(), std::ios::app);
-  }
-  private_nh.param<double>("measurement_range", measurement_range, MAX_MEASUREMENT_RANGE);
-
-  // Publishers
-  filtered_points_pub = nh.advertise<sensor_msgs::PointCloud2>("/filtered_points", 10);
-  points_downsampler_info_pub = nh.advertise<points_downsampler::PointsDownsamplerInfo>("/points_downsampler_info", 1000);
-
-  // Subscribers
-  ros::Subscriber config_sub = nh.subscribe("config/random_filter", 10, config_callback);
-  ros::Subscriber scan_sub = nh.subscribe(POINTS_TOPIC, 10, scan_callback);
-
-  ros::spin();
-
-  return 0;
-}
+} // namespace random_filter
