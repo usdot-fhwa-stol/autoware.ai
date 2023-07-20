5912a834866037b37623b129725c2488d92483b0
Anish <Anishdeva17@gmail.com>
2023-07-20 10:50:27 -0400

    add filepath for ndt_matching logs

--- core_perception/lidar_localizer_ros2/src/ndt_matching.cpp
+++ core_perception/lidar_localizer_ros2/src/ndt_matching.cpp
@@ -129,7 +129,7 @@ static bool _output_log_data = false;
 static std::string _imu_topic = "/imu_raw";
 
 static std::ofstream ofs;
-static std::string filename;  
+static std::string filename = "/opt/carma/ndt_matching.txt";  
 
 static sensor_msgs::msg::Imu imu;
 static nav_msgs::msg::Odometry odom;   
