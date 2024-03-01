1af038d3eaae43874d446fa4ff4df9d225e44728
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-03-01 13:06:14 -0500

    Add ros2 build step

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -32,7 +32,7 @@ while [[ $# -gt 0 ]]; do
       esac
 done
 
-cd ${dir}/src
+cd ${dir}/src/autoware.ai
 
 if [[ "$BRANCH" = "develop" ]]; then
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch $BRANCH
