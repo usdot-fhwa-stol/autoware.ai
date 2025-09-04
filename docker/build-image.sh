ec58e2c84b43e5f578c7bbfb9707f871f59d5061
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-09-04 21:29:15 +0000

    fix

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -129,13 +129,11 @@ cd ..
 if [[ $COMPONENT_VERSION_STRING = "develop" ]]; then
     sed "s|usdotfhwastoldev/|$USERNAME/|g; s|usdotfhwastolcandidate/|$USERNAME/|g; s|usdotfhwastol/|$USERNAME/|g; s|:[0-9]*\.[0-9]*\.[0-9]*|:$COMPONENT_VERSION_STRING|g; s|checkout.bash|checkout.bash -d|g" \
         Dockerfile | docker build -f - --no-cache -t $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING \
-        --progress=plain \
         --build-arg VERSION="$COMPONENT_VERSION_STRING" \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
 elif [[ $COMPONENT_VERSION_STRING = "SNAPSHOT" ]]; then
     docker build --network=host --no-cache -t $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING \
-        --progress=plain \
         --build-arg ROS1_PACKAGES="$ROS1_PACKAGES" \
         --build-arg ROS2_PACKAGES="$ROS2_PACKAGES" \
         --build-arg VERSION="$COMPONENT_VERSION_STRING" \
@@ -146,7 +144,6 @@ else
     #when running the platform inside an Ubuntu 20.04 virtual machine. The error and possible soliutions are
     # discussed here: https://github.com/moby/moby/issues/41003
     docker build --network=host --no-cache -t $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING \
-        --progress=plain \
         --build-arg VERSION="$COMPONENT_VERSION_STRING" \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
